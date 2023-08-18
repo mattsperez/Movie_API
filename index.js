e5d9ddfb68b1012c5b5e7c5a31c85ab450a400fe
mattsperez <131983739+mattsperez@users.noreply.github.com>
2023-08-18 12:36:29 -0500

    updated index

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const express = require('express');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const app = express();
+
+let myLogger = (req, res, next) => {
+    console.log(req.url);
+    next();
+};
+
+let requestTime = (req, res, next) => {
+    req.requestTime = Date.now();
+    next();
+};
 
 let topMovies = [
     {
@@ -43,6 +58,31 @@ let topMovies = [
         director: 'John Doe'
     }
 ];
+
+//
+app.use(myLogger);
+app.use(requestTime);
+
+app.get('/', (req, res) => {
+    let responseText = 'Welcome to my Movie App';
+    responseText += '<small>Requested at: ' + req.requestTime + '<small>';
+    res.send(responseText);
+});
+
+// log.txt file
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), { flags: 'a' });
+
+// bodyparser
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+// morgan common
+app.use(morgan('combined', { stream: accessLogStream }));
+
 // GET requests
 app.get('/', (req, res) => {
     res.send('Welcome to my Movie App')
@@ -57,9 +97,15 @@ app.get('/movies', (req, res) => {
 });
 
 // public folder documentation
-app.use(express.static('public'));
+app.use('/documentation.html', express.static('public'));
+
+// error message
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('oops something broke!');
+});
 
 // listen for requests
 app.listen(8080, () => {
-    console.log('Your app is listening on port 8080');
+    console.log('Your app is listening on Port 8080.');
 });
