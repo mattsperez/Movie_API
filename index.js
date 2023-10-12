2f93351156b88bee93e6dec631054c33f64623dc
mattsperez <131983739+mattsperez@users.noreply.github.com>
2023-10-12 12:41:04 -0500

    removed passport

--- index.js
+++ index.js
@@ -225,7 +225,7 @@ app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (r
 });
 
 // READ All Movies
-app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
+app.get('/movies', async (req, res) => {
     await Movies.find()
         .then((movies) => {
             res.status(201).json(movies);
