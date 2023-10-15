4f1f2223e8dedfba8b67d8e22ec2885c2bbde6fa
mattsperez <131983739+mattsperez@users.noreply.github.com>
2023-10-15 01:30:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({
 // Cors
 const cors = require('cors');
 app.use(cors());
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'http://localhost:61512'];
 app.use(cors({
     origin: (origin, callback) => {
         if (!origin) return callback(null, true);
