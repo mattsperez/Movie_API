d4a3a307fa86bf4f6bbb9378278194cfbe88f2c5
mattsperez <131983739+mattsperez@users.noreply.github.com>
2023-11-24 17:58:58 -0600

    Update index.js

--- index.js
+++ index.js
@@ -267,7 +267,7 @@ app.use((err, req, res, next) => {
 });
 
 // listen for requests
-const port = process.env.PORT || 61367;
+const port = process.env.PORT || 1234;
 app.listen(port, '0.0.0.0', () => {
     console.log('Listening on Port' + port);
 });
